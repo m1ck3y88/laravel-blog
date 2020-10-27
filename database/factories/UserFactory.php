dace3511fc5c03a9b5b005b7176b545116f7795f
Michael Pollard <mickeymakesapps88@gmail.com>
2020-10-27 12:04:38 -0700

    Drop role_id data seeder and add avatar data seeder to UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'username'=> $faker->userName,
         'name' => $faker->name,
-        'role_id'=> $faker->numberBetween(1, 3),
+        'avatar'=> $faker->imageUrl('200', '200'),
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
